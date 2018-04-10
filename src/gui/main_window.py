a478182100b0e2722a848f5803da3c9a5371a234
Tristan Partin <tristan.partin@gmail.com>
2018-04-10 11:38:55 -0500

    Grid of buttons created

--- src/gui/main_window.py
+++ src/gui/main_window.py
@@ -2,8 +2,9 @@ import gi
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
-from .headerbar import HeaderBar
+from .button_grid import ButtonGrid
 from .button_info import ButtonInfo
+from .headerbar import HeaderBar
 
 import logging
 log: logging = logging.getLogger(__name__)
@@ -14,10 +15,11 @@ class MainWindow(Gtk.ApplicationWindow):
     The main application window
     """
 
-    __button_info = None
-    __grid = None
-    __headerbar = None
-    __revealer = None
+    __button_grid: ButtonGrid = None
+    __button_info: ButtonInfo = None
+    __grid: Gtk.Grid = None
+    __headerbar: HeaderBar = None
+    __revealer: Gtk.Revealer = None
 
     def __init__(self):
         Gtk.ApplicationWindow.__init__(self)
@@ -27,9 +29,17 @@ class MainWindow(Gtk.ApplicationWindow):
         self.__headerbar = HeaderBar()
         self.set_titlebar(self.__headerbar)
 
+        main_box = Gtk.Box.new(Gtk.Orientation.HORIZONTAL, 20)
+        main_box.set_property("margin", 10)
+
+        self.__button_grid = ButtonGrid()
+
         self.__button_info = ButtonInfo()
         self.__revealer = Gtk.Revealer(transition_type=Gtk.RevealerTransitionType.SLIDE_LEFT)
         self.__revealer.add(self.__button_info)
         self.__revealer.set_reveal_child(True)
 
-        self.add(self.__revealer)
+        main_box.pack_start(self.__button_grid, True, True, 0)
+        main_box.pack_end(self.__revealer, False, True, 0)
+
+        self.add(main_box)
