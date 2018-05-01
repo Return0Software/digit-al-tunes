b9689990b93598c9be9eb5bb6bd599747414ba81
Joseph Martinsen <joseph@martinsen.com>
2018-04-30 23:06:42 -0500

    Pressing of multiple fingers now plays a unique sound
    Right hand not implemented for this
    JSON format not implemented

--- src/gui/main_window.py
+++ src/gui/main_window.py
@@ -7,7 +7,7 @@ import logging
 import sys
 import vlc
 
-from typing import Any, Dict, Set, Tuple, Union
+from typing import Any, Dict, Set, Tuple, Union, List
 
 sys.path.append(sys.path[0] + "/..")
 
@@ -20,22 +20,29 @@ from .serial_visualizer import SerialVisualizer
 
 log: logging = logging.getLogger(__name__)
 
-LEFT_SOUND = vlc.MediaPlayer()
-RIGHT_SOUND = vlc.MediaPlayer()
+# SOUNDS = {
+#     0: "/home/joseph/Downloads/music/386721_5999918-lq.mp3",
+#     1: "/home/joseph/Downloads/music/386722_5999918-lq.mp3",
+#     2: "/home/joseph/Downloads/music/386723_5999918-lq.mp3",
+#     3: "/home/joseph/Downloads/music/386724_5999918-lq.mp3",
+#     4: "/home/joseph/Downloads/music/386725_5999918-lq.mp3",
+#     5: "/home/joseph/Downloads/music/386726_5999918-lq.mp3",
+#     6: "/home/joseph/Downloads/music/386727_5999918-lq.mp3",
+# }
 
 SOUNDS = {
-    0: "/home/tristan957/Downloads/piano/4409__pinkyfinger__piano-notes-1-octave/68437__pinkyfinger__piano-a.wav",
-    1: "/home/tristan957/Downloads/piano/4409__pinkyfinger__piano-notes-1-octave/68438__pinkyfinger__piano-b.wav",
-    2: "/home/tristan957/Downloads/piano/4409__pinkyfinger__piano-notes-1-octave/68439__pinkyfinger__piano-bb.wav",
-    3: "/home/tristan957/Downloads/piano/4409__pinkyfinger__piano-notes-1-octave/68440__pinkyfinger__piano-c.wav",
-    4: "/home/tristan957/Downloads/piano/4409__pinkyfinger__piano-notes-1-octave/68441__pinkyfinger__piano-c.wav",
-    5: "/home/tristan957/Downloads/piano/4409__pinkyfinger__piano-notes-1-octave/68442__pinkyfinger__piano-d.wav",
-    6: "/home/tristan957/Downloads/piano/4409__pinkyfinger__piano-notes-1-octave/68443__pinkyfinger__piano-e.wav",
-    7: "/home/tristan957/Downloads/piano/4409__pinkyfinger__piano-notes-1-octave/68444__pinkyfinger__piano-eb.wav",
-    8: "/home/tristan957/Downloads/piano/4409__pinkyfinger__piano-notes-1-octave/68445__pinkyfinger__piano-f.wav",
-    9: "/home/tristan957/Downloads/piano/4409__pinkyfinger__piano-notes-1-octave/68446__pinkyfinger__piano-f.wav",
-    10: "/home/tristan957/Downloads/piano/4409__pinkyfinger__piano-notes-1-octave/68447__pinkyfinger__piano-g.wav",
-    11: "/home/tristan957/Downloads/piano/4409__pinkyfinger__piano-notes-1-octave/68448__pinkyfinger__piano-g.wav"
+    0: "/home/joseph/Downloads/piano/68437__pinkyfinger__piano-a.wav",
+    1: "/home/joseph/Downloads/piano/68438__pinkyfinger__piano-b.wav",
+    2: "/home/joseph/Downloads/piano/68439__pinkyfinger__piano-bb.wav",
+    3: "/home/joseph/Downloads/piano/68440__pinkyfinger__piano-c.wav",
+    4: "/home/joseph/Downloads/piano/68441__pinkyfinger__piano-c.wav",
+    5: "/home/joseph/Downloads/piano/68442__pinkyfinger__piano-d.wav",
+    6: "/home/joseph/Downloads/piano/68443__pinkyfinger__piano-e.wav",
+    7: "/home/joseph/Downloads/piano/68444__pinkyfinger__piano-eb.wav",
+    8: "/home/joseph/Downloads/piano/68445__pinkyfinger__piano-f.wav",
+    9: "/home/joseph/Downloads/piano/68446__pinkyfinger__piano-f.wav",
+    10: "/home/joseph/Downloads/piano/68447__pinkyfinger__piano-g.wav",
+    11: "/home/joseph/Downloads/piano/68448__pinkyfinger__piano-g.wav",
 }
 
 
@@ -45,9 +52,9 @@ class MainWindow(Gtk.ApplicationWindow):
     """
 
     __left_button_grid: ButtonGrid = None
-    __right_button_grid: ButtonGrid = None 
+    __right_button_grid: ButtonGrid = None
     __left_event_set: Set[int] = None
-    __right_event_set: Set[int] = None    
+    __right_event_set: Set[int] = None
     __button_info: ButtonInfo = None
     __data: Dict[str, Any] = None
     __serial_visualizer: SerialVisualizer = None
@@ -57,8 +64,20 @@ class MainWindow(Gtk.ApplicationWindow):
     __reveal_button: Gtk.Button = None
     __reveal_image: Gtk.Image = None
 
+    # VLC players
+    __vlc_instance: vlc.Instance = vlc.Instance()
+    __player_left: vlc.MediaPlayer = __vlc_instance.media_player_new()
+    __player_right: vlc.MediaPlayer = __vlc_instance.media_player_new()
+    __left_active_fingers: List[int] = []
+    __right_active_fingers: List[int] = []
+
     def __init__(self):
         Gtk.ApplicationWindow.__init__(self)
+
+        # Create Media sounds TODO: fix me for json
+        self.__left_sounds: List[vlc.Media] = [self.__vlc_instance.media_new(SOUNDS[x]) for x in
+                                          SOUNDS]  # TODO: make this dict again
+        self.__right_sounds: List[vlc.Media] = []
 
         with open("./config/default.json") as f:
             self.__data = json.load(f)
@@ -93,9 +112,9 @@ class MainWindow(Gtk.ApplicationWindow):
         main_grid.attach(Gtk.Label("<big>Left Hand Combinations</big>", use_markup=True),
             0, 0, 1, 1)
         main_grid.attach(Gtk.Label("<big>Right Hand Combinations</big>", use_markup=True),
-            1, 0, 1, 1)        
+            1, 0, 1, 1)
         main_grid.attach(self.__left_button_grid, 0, 1, 1, 1)
-        main_grid.attach(self.__right_button_grid, 1, 1, 1, 1)        
+        main_grid.attach(self.__right_button_grid, 1, 1, 1, 1)
         main_grid.attach(self.__reveal_button, 2, 1, 1, 1)
         main_grid.attach(self.__revealer, 3, 1, 1, 1)
         main_grid.attach(Gtk.Label("Button labels are binary representations of finger "
@@ -124,23 +143,23 @@ class MainWindow(Gtk.ApplicationWindow):
         self.__button_info.set_info("{} {}".format(hand.name.title(), label), **self.__data[key])
 
     def __update_sound(self, sv: SerialVisualizer, hand: int, finger: int, action: int):
-        event_set = self.__left_event_set if hand == Hand.LEFT else self.__right_event_set
+        player = self.__player_left if hand == Hand.LEFT else self.__player_right
+        active_fingers = self.__left_active_fingers if hand == Hand.LEFT else self.__right_active_fingers
+        sounds = self.__left_sounds if hand == Hand.LEFT else self.__right_sounds
+
         finger += 1
-        if finger not in event_set:
-            event_set.add(finger)
-        else:
-            event_set.remove(finger)
+        if finger not in active_fingers and action == Action.PRESSED:
+            active_fingers.append(finger)
+        elif action == Action.RELEASED:
+            del active_fingers[active_fingers.index(finger)]
 
-
-        sound = SOUNDS[sum(event_set)]
-        player = vlc.MediaPlayer(sound)
-        # print(sum(event_set), sound.is_playing())
-        
-        if action == Action.PRESSED and not player.is_playing():
+        print(Hand[hand], sum(active_fingers), active_fingers)
+        if len(active_fingers) > 0:
+            sound = sounds[sum(active_fingers)]
+            player.set_media(sound)
             player.play()
         else:
             player.stop()
             # event_set.mp.pause()
             # FIXME pause and loop back when done
-            # FIXME pause faster than stop
             # FIXME does a thread help this
